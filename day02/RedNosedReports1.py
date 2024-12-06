527544f44240799dbba27ed0d487e1b11f67abad
Fabian Arndt <fabian.arndt96@proton.me>
2024-12-06 10:52:44 +0100

    Transform logic into list comprehension

--- day02/RedNosedReports1.py
+++ day02/RedNosedReports1.py
@@ -1,15 +1,18 @@
 from Reports import reports
 
-safe_reports = 0
 
-for levels in reports:
+def is_safe_report(levels):
     is_increasing = all(levels[i] < levels[i + 1] for i in range(len(levels) - 1))
     is_decreasing = all(levels[i] > levels[i + 1] for i in range(len(levels) - 1))
     has_valid_differences = all(
         1 <= abs(levels[i] - levels[i + 1]) <= 3 for i in range(len(levels) - 1)
     )
 
-    if (is_increasing or is_decreasing) and has_valid_differences:
-        safe_reports += 1
+    return (is_increasing or is_decreasing) and has_valid_differences
 
-print(safe_reports)
+
+def get_safe_reports_count(reports):
+    return sum(1 for levels in reports if is_safe_report(levels))
+
+
+print(get_safe_reports_count(reports))
